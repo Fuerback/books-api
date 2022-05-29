a778fd8f6ef60ace5fac0d928a353e0b3bae2129
fuerback <felipe.fuerback@pismo.io>
2022-05-29 19:26:47 -0300

    implementing sqlite3 methods

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
--- internal/app/infrastructure/db/sqlite3.go
+++ internal/app/infrastructure/db/sqlite3.go
@@ -2,10 +2,13 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/Fuerback/books-api/internal/app/adapter/repository"
+	"github.com/google/uuid"
 )
 
 type sqlite3 struct {
+	DB *sql.DB
 }
 
 func NewSqlite3() DB {
@@ -13,10 +16,117 @@ func NewSqlite3() DB {
 }
 
 func (s *sqlite3) CreateNewBook(ctx context.Context, book repository.NewBook) (string, error) {
-	return "", nil
+	err := s.connectDatabase()
+	defer s.DB.Close()
+	if err != nil {
+		return "", err
+	}
+
+	tx, err := s.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return "", err
+	}
+
+	stmt, err := tx.PrepareContext(ctx, "insert into book(id, title, author, pages) values (?,?,?,?)")
+	defer stmt.Close()
+	if err != nil {
+		return "", err
+	}
+
+	bookID := uuid.NewString()
+	_, err = stmt.ExecContext(ctx, bookID, book.Title, book.Author, book.Pages)
+	if err != nil {
+		tx.Rollback()
+		return "", err
+	}
+	tx.Commit()
+
+	return bookID, nil
 }
 func (s *sqlite3) FindBook(ctx context.Context, bookID string) (repository.BookDetails, error) {
-	return repository.BookDetails{}, nil
+	err := s.connectDatabase()
+	defer s.DB.Close()
+	if err != nil {
+		return repository.BookDetails{}, err
+	}
+
+	stmt, err := s.DB.PrepareContext(ctx, "select * from book where id = ? and deleted = 0")
+	defer stmt.Close()
+	if err != nil {
+		return repository.BookDetails{}, err
+	}
+
+	book := repository.BookDetails{}
+	err = stmt.QueryRow(bookID).Scan(&book.ID, &book.Title, &book.Author, &book.Pages)
+	if err != nil {
+		return repository.BookDetails{}, err
+	}
+
+	return book, nil
 }
-func (s *sqlite3) UpdateBook(ctx context.Context, book repository.NewBook) error { return nil }
-func (s *sqlite3) DeleteBook(ctx context.Context, bookID string) error           { return nil }
+func (s *sqlite3) UpdateBook(ctx context.Context, book repository.NewBook) error {
+	err := s.connectDatabase()
+	defer s.DB.Close()
+	if err != nil {
+		return err
+	}
+
+	tx, err := s.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.PrepareContext(ctx, "update book set title = ?, author = ?, pages = ? where id = ? and deleted = 0")
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+
+	bookID := uuid.NewString()
+	_, err = stmt.ExecContext(ctx, book.Title, book.Author, book.Pages, bookID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+
+	return nil
+}
+
+func (s *sqlite3) DeleteBook(ctx context.Context, bookID string) error {
+	err := s.connectDatabase()
+	defer s.DB.Close()
+	if err != nil {
+		return err
+	}
+
+	tx, err := s.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.PrepareContext(ctx, "update book set deleted = 1 where id = ?")
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.ExecContext(ctx, bookID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+
+	return nil
+}
+
+func (s *sqlite3) connectDatabase() error {
+	db, err := sql.Open("sqlite3", "./book.db")
+	if err != nil {
+		return err
+	}
+
+	s.DB = db
+	return nil
+}
